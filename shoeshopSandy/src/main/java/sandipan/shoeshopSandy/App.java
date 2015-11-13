e799a8aa5b496cd62b0100ecdd18eee0c36f8bf3
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-13 15:58:53 +0530

    initial commit

---
+++ shoeshopSandy/src/main/java/sandipan/shoeshopSandy/App.java
@@ -0,0 +1,13 @@
+package sandipan.shoeshopSandy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
