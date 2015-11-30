93a40f158059914fcdbfd0e7e8437e0b298818fa
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-30 11:56:35 +0530

    Renamed and add search test

---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/smoketest/SearchAShoeTest.java
@@ -0,0 +1,26 @@
+package sandipan.shoeshopSandy.smoketest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchAShoeTest {
+
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Search_A_Shoe_Test()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
