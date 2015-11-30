6ffa66d6e5f426bc9db4d4ecf60d581f8b078f42
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-30 15:35:08 +0530

    Write all possible test skeleton for women shoe landing page test

---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/shopping/women/WomenLandingPageGridViewTest.java
@@ -0,0 +1,26 @@
+package sandipan.shoeshopSandy.shopping.women;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WomenLandingPageGridViewTest {
+
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Test()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/shopping/women/WomenLandingPageSubcategoriesNavigationTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package sandipan.shoeshopSandy.shopping.women;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sandipan
+ *
+ */
+public class WomenLandingPageSubcategoriesNavigationTest {
+
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Test()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
