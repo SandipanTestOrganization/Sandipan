5484ca84a84cacefd4c10e49eab5c97fd47b4ac6
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-30 11:44:49 +0530

    Rename HomePAge to Home Page Test and put a basic home page test skeliton

---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/smoketest/HomePageTest.java
@@ -0,0 +1,26 @@
+package sandipan.shoeshopSandy.smoketest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HomePageTest {
+	
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Go_To_Home_Page()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
