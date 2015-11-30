c6da975664c9ad69c082b864e8bd4ae007755032
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-30 16:09:57 +0530

    Logger class details added

--- shoeshopSandy/src/test/java/sandipan/shoeshopSandy/smoketest/HomePageTest.java
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/smoketest/HomePageTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sandipan.shoeshopSandy.browser.Browser;
+import sandipan.shoeshopSandy.logger.SHOELogger;
 import sandipan.shoeshopSandy.pages.HomePage;
 
 public class HomePageTest {
