aabef6046b9e4843694ae0380d0dcab7e4ef7983
Sandipan Pramanik <sandipan.mca@gmail.com>
2015-11-30 15:43:05 +0530

    Add all possible men shoe related test skeleton

---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/shopping/men/MenLandingPageSubcategoriesNavigationTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package sandipan.shoeshopSandy.shopping.men;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sandipan
+ *
+ */
+public class MenLandingPageSubcategoriesNavigationTest {
+
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Test()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
---
+++ shoeshopSandy/src/test/java/sandipan/shoeshopSandy/shopping/men/MenShoeDetailsTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package sandipan.shoeshopSandy.shopping.men;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sandipan
+ *
+ */
+public class MenShoeDetailsTest {
+
+	@Before
+	public void Init()
+	{
+		
+	}
+	
+	@Test
+	public void Can_Test()
+	{
+		
+	}
+
+	@After
+	public void CleanUp()
+	{
+		
+	}
+}
